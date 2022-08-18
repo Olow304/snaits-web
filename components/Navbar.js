5eb2c1b2e5cf59cd917b7da79400ad33ced6d3aa
saleban olow <olow304@gmail.com>
2022-08-18 10:51:24 -0500

    feat: add bg color and first commit on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar(props) {
     return (
-        <div>
+        <div className='bg-gray-800 h-[50px] text-white flex items-center px-6'>
             <span>Navbar goes here</span>
         </div>
     );
